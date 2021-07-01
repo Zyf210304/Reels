03bba5068c96f0389f9d3d207b7d81c7d1b54f34
张亚飞 <672233580@qq.com>
2021-07-01 22:58:13 +0800

    Initial Commit

---
+++ Reels/ReelsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReelsApp.swift
+//  Reels
+//
+//  Created by 张亚飞 on 2021/7/1.
+//
+
+import SwiftUI
+
+@main
+struct ReelsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
